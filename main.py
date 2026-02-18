8a7012b496ede4963b871335fd2e199cf09cb4da
Aiman <aimanelkhamis@yahoo.de>
2026-02-18 11:30:13 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.title("Catan Analyse")
+Ressourcen = ["Stein", "Getreide", "Schaf", "Holz", "Lehm"]
+
+# Kopfzeile
+header = st.columns(6)
+header[0].write("Ressource")
+for i in range(1, 6):
+    header[i].write(f"Karte {i}")
+
+# Tabellenzeilen
+for ressource in Ressourcen:
+    cols = st.columns(6)
+
+    cols[0].write(ressource)
+
+    for i in range(1, 6):
+        cols[i].text_input(
+            "",
+            key=f"{ressource}_{i}",
+            label_visibility="collapsed"
+        )
+
+st.session_state
