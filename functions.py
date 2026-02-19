960b6fe3508782017c6e8af47268eb8268d214b8
Aiman <aimanelkhamis@yahoo.de>
2026-02-19 20:06:56 +0100

    Erste VErsion

--- functions.py
+++ functions.py
@@ -1,1 +1,15 @@
+list=[6, 0, 0, 0, 0]
+
+def Score(Liste):
+    liste_Werte=[]
+    for list in Liste:
+        if list<7 and list >1:
+            wert=list-1
+        elif list==7 or list==1 or list==0:
+            wert=0
+        else:
+            wert=13-list
+        liste_Werte.append(wert)
+    Summe=sum(liste_Werte)
+    return Summe
 
--- main.py
+++ main.py
@@ -1,24 +1,55 @@
 import streamlit as st
+import functions as func
 
 Ressourcen = ["Stein", "Getreide", "Schaf", "Holz", "Lehm"]
 
 # Kopfzeile
-header = st.columns(6)
+header = st.columns(7)
 header[0].write("Ressource")
 for i in range(1, 6):
     header[i].write(f"Karte {i}")
 
 # Tabellenzeilen
 for ressource in Ressourcen:
-    cols = st.columns(6)
+    cols = st.columns(7)
 
     cols[0].write(ressource)
 
     for i in range(1, 6):
-        cols[i].text_input(
+        cols[i].number_input(
             "",
             key=f"{ressource}_{i}",
-            label_visibility="collapsed"
+            label_visibility="collapsed",
+            min_value = 0,
+            max_value = 12,
+            step = 1,
+            value=0
         )
 
-st.session_state
+
+werte=[]
+Summe=0
+Ergebnisse=[]
+Final=[]
+
+# Ergebnis
+
+
+if st.button("Rechne"):
+    Ergebnis = st.columns(6)
+
+    for Ressource in Ressourcen:
+        for i in range (1, 6):
+            werte.append(st.session_state.get(f"{Ressource}_{i}", ""))
+        Summe=func.Score(werte)
+        Ergebnisse.append(Summe)
+        werte=[]
+        Summe=0
+
+    Final = list(zip(Ressourcen, Ergebnisse))
+
+    for i in range(0,5):
+        Ergebnis[i].write(f"{Final[i][0]} : {Final[i][1]}")
+
+
+
